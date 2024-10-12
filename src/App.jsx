d22b7662d1889e2bc46f9076ef83a2a57fc98901
Eduardo S. Libardi <eslibardi@gmail.com>
2024-10-12 16:35:10 -0300

    Setup Initial Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from './pages';
+
 const App = () => {
   return <h1 className='text-7xl font-bold underline'>Tailwind project</h1>;
 };
---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,4 @@
+const Checkout = () => {
+  return <h1 className='text-4xl'>Checkout </h1>;
+};
+export default Checkout;
