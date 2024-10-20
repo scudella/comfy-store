b6e980c7350822dfb887671076ad6969882ab94a
Eduardo S. Libardi <eslibardi@gmail.com>
2024-10-20 12:46:42 -0300

    Hero component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,10 @@
+import { Hero } from '../components';
+
 const Landing = () => {
-  return <h1 className='text-4xl'>Landing </h1>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 export default Landing;
