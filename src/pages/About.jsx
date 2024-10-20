b4d1fe97fd86ede30b44854886d3365a2d1e62d4
Eduardo S. Libardi <eslibardi@gmail.com>
2024-10-20 12:10:14 -0300

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,25 @@
 const About = () => {
-  return <h1 className='text-4xl'>About</h1>;
+  return (
+    <>
+      <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
+        <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
+          We love
+        </h1>
+        <div className='stats bg-primary shadow'>
+          <div className='stat'>
+            <div className='stat-title text-primary-content text-4xl font-bold tracking widest'>
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className='mt-6 text-lg leading-8 max-w-2xl mx-auto'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        adipisci quibusdam et praesentium iste libero voluptatibus ab aliquam
+        est ex officiis non modi dolore quod, iure vero, culpa nemo velit fugiat
+        vitae sit asperiores error? Odit quia expedita asperiores placeat!
+      </p>
+    </>
+  );
 };
 export default About;
