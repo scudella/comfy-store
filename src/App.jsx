6f4f7ac1ff6f0c6382470e58ec62bc4359e041d3
Eduardo S. Libardi <eslibardi@gmail.com>
2024-10-27 16:34:50 -0300

    Single Product Page

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import { ErrorElement } from './components';
 
 // loaders
 import { loader as landingLoader } from './pages/Landing';
+import { loader as singleProductLoader } from './pages/SingleProduct';
 
 //actions
 
@@ -40,6 +41,8 @@ const router = createBrowserRouter([
       {
         path: 'products/:id',
         element: <SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader: singleProductLoader,
       },
       {
         path: 'cart',
