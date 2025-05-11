9c5d35b63ea9a02a808246af7082d7f01c3507dc
Eduardo S. Libardi <eslibardi@gmail.com>
2025-05-11 10:37:45 -0300

    Loading ring

---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <span className='loading-ring loading-lg, loading'></span>
+    </div>
+  );
+};
+export default Loading;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,14 +1,21 @@
-import { Outlet } from 'react-router-dom';
-import { Header, Navbar } from '../components';
+import { Outlet, useNavigation } from 'react-router-dom';
+import { Header, Navbar, Loading } from '../components';
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPagingLoading = navigation.state === 'loading';
+
   return (
     <>
       <Header />
       <Navbar />
-      <section className='align-element py-20'>
-        <Outlet />
-      </section>
+      {isPagingLoading ? (
+        <Loading />
+      ) : (
+        <section className='align-element py-20'>
+          <Outlet />
+        </section>
+      )}
     </>
   );
 };
