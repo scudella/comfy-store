b6e9c6098cadf50a9850089b5422be1b78202279
Eduardo S. Libardi <eslibardi@gmail.com>
2025-05-11 10:49:35 -0300

    Filter params sent to backend and default values

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,38 +5,58 @@ import FormRange from './FormRange';
 import FormCheckbox from './FormCheckbox';
 
 const Filters = () => {
-  const { meta } = useLoaderData();
+  const { meta, params } = useLoaderData();
+  const { search, company, category, shipping, order, price } = params;
   return (
     <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
+      {/* Search */}
       <FormInput
         type='search'
         label='search product'
         name='search'
         size='input-sm'
+        defaultValue={search}
       />
+      {/* Category */}
       <FormSelect
         label='select category'
         name='category'
         list={meta.categories}
         size='select-sm'
+        defaultValue={category}
       />
+      {/* Company */}
       <FormSelect
         label='select company'
         name='company'
         list={meta.companies}
         size='select-sm'
+        defaultValue={company}
       />
+      {/* Order */}
       <FormSelect
         label='sort by'
         name='order'
         list={['a-z', 'z-a', 'high', 'low']}
         size='select-sm'
+        defaultValue={order}
+      />
+      {/* Price */}
+      <FormRange
+        name='price'
+        label='select price'
+        size='range-sm'
+        price={price}
+      />
+      {/* Shipping */}
+      <FormCheckbox
+        name='shipping'
+        label='free shipping'
+        size='checkbox-sm'
+        defaultValue={shipping}
       />
 
-      <FormRange name='price' label='select price' size='range-sm' />
-
-      <FormCheckbox name='shipping' label='free shipping' size='checkbox-sm' />
-
+      {/* Buttons */}
       <button type='submit' className='btn btn-primary btn-sm uppercase'>
         search
       </button>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,16 @@
 import { Filters, PaginationContainer, ProductsContainer } from '../components';
 import { customFetch } from '../utils';
 
-const URL = '/products';
+const url = '/products';
 
 export const loader = async ({ request }) => {
-  const response = await customFetch(URL);
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  const response = await customFetch(url, { params });
   const products = response.data.data;
   const meta = response.data.meta;
-  return { products, meta };
+  return { products, meta, params };
 };
 
 const Products = () => {
