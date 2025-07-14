41ad04d54d414fff01a9cd8c10ca597f7875d4c0
Eduardo S. Libardi <eslibardi@gmail.com>
2025-07-14 10:44:40 -0300

    login user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,48 @@
-import { FormInput, SubmitBtn } from '../components';
-import { Form, Link } from 'react-router-dom';
+import {FormInput, SubmitBtn} from '../components'
+import {Form, Link, redirect, useNavigate} from 'react-router-dom'
+import {customFetch} from '../utils'
+import {toast} from 'react-toastify'
+import {loginUser} from '../features/user/userSlice'
+import {useDispatch} from 'react-redux'
+
+export const action =
+  (store) =>
+  async ({request}) => {
+    const formData = await request.formData()
+    const data = Object.fromEntries(formData)
+
+    try {
+      const response = await customFetch.post('/auth/local', data)
+      store.dispatch(loginUser(response.data))
+      toast.success('logged in successfully')
+      return redirect('/')
+    } catch (error) {
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        'please double check your credentials'
+      toast.error(errorMessage)
+      return null
+    }
+  }
 
 const Login = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const loginAsGuestUser = async () => {
+    try {
+      const response = await customFetch.post('/auth/local', {
+        identifier: 'test@test.com',
+        password: 'secret',
+      })
+      dispatch(loginUser(response.data))
+      toast.success('Welcome guest user')
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+      toast.error('Guest user login error. Please try again')
+    }
+  }
   return (
     <section className='h-screen grid place-items-center'>
       <Form
@@ -14,7 +55,6 @@ const Login = () => {
           label='email'
           name='identifier'
           placeholder='Enter your email'
-          defaultValue='test@test.com'
         />
 
         <FormInput
@@ -22,12 +62,15 @@ const Login = () => {
           label='password'
           name='password'
           placeholder='Enter your password'
-          defaultValue='secret'
         />
         <div className='mt-4'>
           <SubmitBtn text='login' />
         </div>
-        <button type='button' className='btn btn-secondary btn-block uppercase'>
+        <button
+          type='button'
+          className='btn btn-secondary btn-block uppercase'
+          onClick={loginAsGuestUser}
+        >
           guest user
         </button>
         <p className='text-center'>
@@ -41,6 +84,6 @@ const Login = () => {
         </p>
       </Form>
     </section>
-  );
-};
-export default Login;
+  )
+}
+export default Login
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,7 +8,7 @@ export const action = async ({request}) => {
   const data = Object.fromEntries(formData)
 
   try {
-    const response = await customFetch.post('/auth/local/register', data)
+    await customFetch.post('/auth/local/register', data)
     toast.success('account created successfully')
     return redirect('/login')
   } catch (error) {
